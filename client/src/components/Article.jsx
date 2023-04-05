ef3667958e51041a462056696268ded9b38ae723
AzriHaniz23 <azriqwer@gmail.com>
2023-04-05 11:26:59 +0800

    forum by Azri Haniz

--- client/src/components/Article.jsx
+++ client/src/components/Article.jsx
@@ -1,23 +1,29 @@
 const Article = () => {
-    return (
-        <div>
-        <div class="banner-area">
-                <h2>Mental Health Title</h2>
-                <h3>...subtitle and more words here</h3>
-            </div>
-        
-            <div class="content-area">
-                <div class="content">
-                    <h2>Words here</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sequi modi, ducimus culpa aspernatur veritatis laboriosam odit maiores dolore, perferendis qui cum totam quaerat doloribus consectetur voluptate debitis velit quisquam?
-                    </p>
-                    <h2>More words here</h2>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel fugit deserunt iusto. Aperiam molestiae, voluptatum saepe sapiente quisquam, est officiis aliquid consectetur placeat dolorem sit fugit hic, quod accusantium nihil?
-                    </p>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="banner-area">
+        <h2>Mental Health Title</h2>
+        <h3>...subtitle and more words here</h3>
+      </div>
+
+      <div className="content-area">
+        <div className="content">
+          <h2>Words here</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sequi
+            modi, ducimus culpa aspernatur veritatis laboriosam odit maiores
+            dolore, perferendis qui cum totam quaerat doloribus consectetur
+            voluptate debitis velit quisquam?
+          </p>
+          <h2>More words here</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel fugit
+            deserunt iusto. Aperiam molestiae, voluptatum saepe sapiente
+            quisquam, est officiis aliquid consectetur placeat dolorem sit fugit
+            hic, quod accusantium nihil?
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
