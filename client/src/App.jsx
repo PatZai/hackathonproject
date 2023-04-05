084b4c577c0d468bf94017fdcab49cef04dd4840
haazimhadzid <haazimhadzid@gmail.com>
2023-04-05 12:07:05 +0800

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/App.jsx 	modified:   client/src/index.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 // import './App.css';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {HashRouter, Route,Routes } from 'react-router-dom';
+
 
 function App() {
 	return (
-		<Router>
+		// <Router>
+		<HashRouter>
 			<Routes>
+			{/* <div> */}
 				<Route path="/" element={<HomePage />} />
 				<Route path="/Register" element={<RegisterPage />} />
-			</Routes>
-		</Router>
+				</Routes>
+			{/* </div> */}
+		</HashRouter>
+		// </Router>
 	);
 }
 
