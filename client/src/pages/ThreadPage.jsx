a780c5e09a4e002333ad46bd732a926ca21032b6
PatrickZai <azri.patrick@gmail.com>
2023-04-05 14:49:18 +0800

    threadpage

---
+++ client/src/pages/ThreadPage.jsx
@@ -0,0 +1,15 @@
+import NavbarList from "../components/NavbarList";
+import Thread from "../components/Thread";
+import Footers from "../components/Footers";
+
+const ThreadPage = () => {
+    return (
+        <>
+            <NavbarList />
+            <Thread />
+            <Footers />
+        </>
+    )
+}
+
+export default ThreadPage;
