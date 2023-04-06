4d5d068b5a7ad5ee317ceee8d7f41886a583c752
PatrickZai <azri.patrick@gmail.com>
2023-04-06 18:55:21 +0800

    New updates

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import FooterComponent from '../components/Footers/Footers';
 import NavbarList from '../components/Navbar/NavbarList';
 import CardList from '../components/CardList/CardList';
 import Header from '../components/Header/Header';
-import TrendingThread from '../components/Threads/TrendingThread';
+import TrendingThread from '../components/TrendingThread/TrendingThread';
 
 const HomePage = () => {
 	return (
