70b121905f726e531166ea2291768d947e13227e
haazimhadzid <haazimhadzid@gmail.com>
2023-04-06 20:16:38 +0800

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/components/Footers/style.css 	modified:   client/src/components/Login/Login.jsx 	modified:   client/src/components/Navbar/NavbarList.jsx 	modified:   client/src/components/Survey/Survey.jsx 	deleted:    client/src/components/Survey/style.css 	new file:   client/src/components/Survey/survey.css 	modified:   client/src/index.jsx 	modified:   client/src/pages/SurveyPage.jsx

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,49 +1,51 @@
-import React, { useState } from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export const Login = (props) => {
-  const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+	const [email, setEmail] = useState('');
+	const [pass, setPass] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email);
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(email);
+	};
 
-  return (
-    <div className="login">
-    <div className="auth-form-container">
-      <h2>Login</h2>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email / User name</label>
-        <input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="email@gmail.com / user name"
-          id="email"
-          name="email"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          value={pass}
-          onChange={(e) => setPass(e.target.value)}
-          type="password"
-          placeholder="********"
-          id="password"
-          name="password"
-        />
-        <button type="submit">Log In</button>
-      </form>
-      <button
-        className="link-btn"
-        onClick={() => props.onFormSwitch("register")}
-      >
-        Don't have an account? Register here.
-      </button>
-    </div>
-    </div>
-  );
+	return (
+		<div className="login">
+			<div className="auth-form-container">
+				<h2>Login</h2>
+				<form className="login-form" onSubmit={handleSubmit}>
+					<label htmlFor="email">Email / User name</label>
+					<input
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						type="email"
+						placeholder="email@gmail.com / user name"
+						id="email"
+						name="email"
+					/>
+					<label htmlFor="password">Password</label>
+					<input
+						value={pass}
+						onChange={(e) => setPass(e.target.value)}
+						type="password"
+						placeholder="********"
+						id="password"
+						name="password"
+					/>
+					<button type="submit" onclick="myFunction()">
+						Log In
+					</button>
+				</form>
+				<button
+					className="link-btn"
+					onClick={() => props.onFormSwitch('register')}
+				>
+					Don't have an account? Register here.
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- client/src/pages/SurveyPage.jsx
+++ client/src/pages/SurveyPage.jsx
@@ -1,6 +1,6 @@
 import NavbarList from '../components/Navbar/NavbarList';
 import Footers from '../components/Footers/Footers';
-import Survey from '../components/Survey/Survey';
+import Survey from '../components/Survey/Survey.jsx';
 
 import React from 'react';
 
