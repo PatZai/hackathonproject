7ae1663f7ad3164e660d68a3cde61bd5fe5adc81
PatrickZai <azri.patrick@gmail.com>
2023-04-06 12:41:12 +0800

    Change to thread css and made new trendingthreads.jsx

--- client/src/components/Threads/TrendingThread.jsx
+++ client/src/components/Threads/TrendingThread.jsx
@@ -32,60 +32,67 @@ const TrendingThread = () => {
                             </button>
                         </div>
                     </div>
-                    <div class="comment2" id="comment-2">
-                        <div class="comment-heading">
-                            <img src=" " alt="test" class="pfp" />
-                            <div class="comment-info">
-                                <a href=" " class="comment-author">username2</a>
-                            </div>
+                </div>
+                
+            </div>
+            <div class="comment-thread">
+                <div class="comment1" id="comment-1">
+                    <div class="comment-heading">
+                        <img src=" " alt="test" class="pfp" />
+                        <div class="comment-info">
+                            <a href=" " class="comment-author">username1</a>
                         </div>
-                        <div class="comment-body">
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptatum. Quae animi, ipsum illum accusamus eligendi incidunt, sapiente quia itaque natus aut cum exercitationem cupiditate velit. Vitae vel expedita unde.
+                    </div>
+
+                    <div class="comment-body">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptatum. Quae animi, ipsum illum accusamus eligendi incidunt, sapiente quia itaque natus aut cum exercitationem cupiditate velit. Vitae vel expedita unde.
+                        </p>
+                        <div class="comment-voting">
+                            <p class="m-0">
+                                xx points &bull; x days ago
                             </p>
-                            <div class="comment-voting">
-                                <p class="m-0">
-                                    xx points &bull; x days ago
-                                </p>
-                                <button type="button">
-                                    <span aria-hidden="true">&#9650;</span>
-                                    <span class="sr-only">Vote up</span>
-                                </button>
-                                <button type="button">
-                                    <span aria-hidden="true">&#9660;</span>
-                                    <span class="sr-only">Vote down</span>
-                                    <button type="button">Reply</button>
-                                    <button type="button">Flag</button>
-                                </button>
-                            </div>
+                            <button type="button">
+                                <span aria-hidden="true">&#9650;</span>
+                                <span class="sr-only">Vote up</span>
+                            </button>
+                            <button type="button">
+                                <span aria-hidden="true">&#9660;</span>
+                                <span class="sr-only">Vote down</span>
+                                <button type="button">Reply</button>
+                                <button type="button">Flag</button>
+                            </button>
                         </div>
-                        <div class="comment3" id="comment-3">
-                            <div class="comment-heading">
-                                <img src=" " alt="test" class="pfp" />
-                                <div class="comment-info">
-                                    <a href=" " class="comment-author">username3</a>
-                                </div>
-                            </div>
-                            <div class="comment-body">
-                                <p>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptatum. Quae animi, ipsum illum accusamus eligendi incidunt, sapiente quia itaque natus aut cum exercitationem cupiditate velit. Vitae vel expedita unde.
-                                </p>
-                                <div class="comment-voting">
-                                    <p class="m-0">
-                                        xx points &bull; x days ago
-                                    </p>
-                                    <button type="button">
-                                        <span aria-hidden="true">&#9650;</span>
-                                        <span class="sr-only">Vote up</span>
-                                    </button>
-                                    <button type="button">
-                                        <span aria-hidden="true">&#9660;</span>
-                                        <span class="sr-only">Vote down</span>
-                                        <button type="button">Reply</button>
-                                        <button type="button">Flag</button>
-                                    </button>
-                                </div>
-                            </div>
+                    </div>
+                </div>
+            </div>
+            <div class="comment-thread">
+                <div class="comment1" id="comment-1">
+                    <div class="comment-heading">
+                        <img src=" " alt="test" class="pfp" />
+                        <div class="comment-info">
+                            <a href=" " class="comment-author">username1</a>
+                        </div>
+                    </div>
+
+                    <div class="comment-body">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, voluptatum. Quae animi, ipsum illum accusamus eligendi incidunt, sapiente quia itaque natus aut cum exercitationem cupiditate velit. Vitae vel expedita unde.
+                        </p>
+                        <div class="comment-voting">
+                            <p class="m-0">
+                                xx points &bull; x days ago
+                            </p>
+                            <button type="button">
+                                <span aria-hidden="true">&#9650;</span>
+                                <span class="sr-only">Vote up</span>
+                            </button>
+                            <button type="button">
+                                <span aria-hidden="true">&#9660;</span>
+                                <span class="sr-only">Vote down</span>
+                                <button type="button">Reply</button>
+                                <button type="button">Flag</button>
+                            </button>
                         </div>
                     </div>
                 </div>
