1867579d6f9d5f136d43772e5ee6570cffa56f9b
haazimhadzid <haazimhadzid@gmail.com>
2023-04-07 08:35:22 +0800

    modified:   client/src/components/CardList/CardList.jsx

--- client/src/components/CardList/CardList.jsx
+++ client/src/components/CardList/CardList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import { Link } from 'react-router-dom';
 
 const cardList = () => {
 	return (
@@ -13,28 +14,28 @@ const cardList = () => {
 					<div class="content">
 						<h2 class="title">Anxiety</h2>
 						<p class="copy">Learn more about how to deal with anxiety...</p>
-						<button class="btn">Click here</button>
+						<button class="btn"><Link to="/Anxiety">Click here</Link></button>
 					</div>
 				</div>
 				<div class="card">
 					<div class="content">
 						<h2 class="title">Depression</h2>
 						<p class="copy">Learn more about how to deal with depression...</p>
-						<button class="btn">Click here</button>
+						<button class="btn"><Link to="/Depression">Click here</Link></button>
 					</div>
 				</div>
 				<div class="card">
 					<div class="content">
 						<h2 class="title">Phobias</h2>
 						<p class="copy">Learn more about how to deal with phobias...</p>
-						<button class="btn">Click here</button>
+						<button class="btn"><Link to="/Phobias">Click here</Link></button>
 					</div>
 				</div>
 				<div class="card">
 					<div class="content">
 						<h2 class="title">Trauma</h2>
 						<p class="copy">Learn more about how to deal with phobias...</p>
-						<button class="btn">Click here</button>
+						<button class="btn"><Link to="/Trauma">Click here</Link></button>
 					</div>
 				</div>
 			</main>
